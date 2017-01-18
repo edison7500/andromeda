d9c4ae271d070844669878fc8b192f8f4b6b703b
jiaxin <edison7500@gmail.com>
2017-01-18 09:18:32 +0800

    fixed bug

--- andromeda/middlewares/downloader.py
+++ andromeda/middlewares/downloader.py
@@ -11,20 +11,20 @@ import logging
 class PhantomJSDownloader(object):
 
     def process_request(self, request, spider):
+        response    = None
         if spider.name in settings.PHANTOMJS_SPIDER:
             browser = webdriver.PhantomJS(executable_path=settings.PHANTOMJS_PATH)
-
             try:
                 browser.set_window_size(800, 600)
                 browser.get(request.url)
                 content     = browser.page_source.encode("utf-8")
                 url         = browser.current_url.encode('utf-8')
-                # browser.quit()
-                return HtmlResponse(url, encoding='utf-8', status=200, body=content)
+                response = HtmlResponse(url, encoding='utf-8', status=200, body=content)
             except Exception as e:
                 logging.error(e.message)
             finally:
                 browser.quit()
+            return response
 
 
     def process_response(self, request, response, spider):
