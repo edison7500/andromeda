688c92ff6e6d81dc04fe3dd44ce83d7064acbe5c
jiaxin <edison7500@gmail.com>
2018-01-26 13:54:19 +0800

    update

--- github/setup.py
+++ github/setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'project',
-    version      = '1.0',
-    packages     = find_packages(),
-    entry_points = {'scrapy': ['settings = github.settings']},
+    name='project',
+    version='1.1',
+    packages=find_packages(),
+    entry_points={'scrapy': ['settings = github.settings']},
 )
